d553878955e10851c0927723b72a079fbe489842
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2026-06-28 18:53:38 -0300

    end-point carro

--- arquiteturaspring/src/main/java/arquiteturaspring/api/TesteFabricaController.java
+++ arquiteturaspring/src/main/java/arquiteturaspring/api/TesteFabricaController.java
@@ -3,22 +3,26 @@ package arquiteturaspring.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import arquiteturaspring.model.Carro;
+import arquiteturaspring.model.CarroStatus;
 import arquiteturaspring.model.Chave;
 import arquiteturaspring.model.Motor;
 import arquiteturaspring.model.Santana;
 
 @RestController
+@RequestMapping("/carros")
 public class TesteFabricaController {
 	
 	@Autowired
 	private Motor motor; 
 	
 	@PostMapping
-	public void ligar(@RequestBody Chave chave) {
+	public CarroStatus ligar(@RequestBody Chave chave) {
 		Carro carro = new Santana(motor);
+		return carro.darIgnicao(chave);
 	}
 
 }
